47c3c759a90225d9fa9d6d9b682db59c100f77b2
FrostySoulle <saoodkeriwala@gmail.com>
2026-06-23 18:26:09 +0530

    Research and hypotheses webpages

---
+++ dashboard/assets/js/app.js
@@ -0,0 +1,14 @@
+loadMarkdown(
+    "/docs/research_questions.md",
+    "questions-container"
+);
+
+loadMarkdown(
+    "/docs/research_results.md",
+    "results-container"
+);
+
+loadMarkdown(
+    "/docs/decision_log.md",
+    "decision-container"
+);
